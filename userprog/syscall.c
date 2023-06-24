370fb7abc9cd7046092e012454229d92fd170273
kr-younghoon <sullanta0802@gmail.com>
2023-06-24 14:27:13 +0000

    Add: munmap() 시스템 콜 구현

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -119,6 +119,9 @@ void syscall_handler(struct intr_frame *f UNUSED) {
 	case SYS_MMAP:
 		f->R.rax = mmap(f->R.rdi, f->R.rsi, f->R.rdx, f->R.r10, f->R.r8);
 		break;
+	case SYS_MUNMAP:
+		munmap(f->R.rdi);
+		break;
 	}
 }
 
@@ -333,4 +336,9 @@ mmap(void *addr, size_t length, int writable, int fd, off_t offset) {
 		return NULL;
 	
 	return do_mmap(addr, length, writable, f, offset);
+}
+
+void
+munmap(void *addr) {
+	do_munmap(addr);
 }
